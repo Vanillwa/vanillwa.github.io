d5ea10efa879a8ed6ffbf502af53f60d15a23f79
Vanillwa <yjh30448813@gmail.com>
2024-09-15 21:32:02 +0900

    다크모드버튼

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import Header from "./components/header";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.theme);
+
   return (
-    <div className='App'>
-      <Header />
+    <>
+      <Header theme={theme} setTheme={setTheme} />
       <div>포트폴리오 올릴 예정</div>
-    </div>
+    </>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,42 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown, ToggleButton } from "react-bootstrap";
+import styles from "../css/Header.module.css";
 
-function header() {
+function Header(props) {
+  const { theme, setTheme } = props;
+  const [checked, setChecked] = useState(false);
+  const handleThemeChange = (e) => {
+    setChecked(e.currentTarget.checked);
+    if (theme === "dark") {
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    } else {
+      setTheme("dark");
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    }
+  };
   return (
-    <Navbar expand='lg' className='bg-body-tertiary'>
+    <Navbar expand='lg' className='bg-body-tertiary' data-bs-theme={theme}>
       <Container>
-        <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href='#home'>윤정현 - 포트폴리오</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
-            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>Another action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href='#home'>About Me</Nav.Link>
+            <Nav.Link href='#link'>Activities</Nav.Link>
+            <Nav.Link href='#link'>Technical Skills</Nav.Link>
+            <Nav.Link href='#link'>Projects</Nav.Link>
+            <Nav.Link href='#link'>Certifications</Nav.Link>
           </Nav>
+          <ToggleButton id='toggle-check' type='checkbox' variant='outline-primary' checked={checked} value='1' onChange={(e) => handleThemeChange(e)}>
+            {theme}
+          </ToggleButton>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default header;
+export default Header;
