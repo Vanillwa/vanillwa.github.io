cbd3fe61a5c8e6805a7a17729173f533e9824eb9
Vanillwa <yjh30448813@gmail.com>
2024-09-16 21:48:01 +0900

    다크모드버튼 아이콘

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.theme);
-
+  useEffect(() => {
+    if (localStorage.theme === "dark") document.documentElement.classList.add("dark");
+  }, []);
   return (
     <>
+      <section></section>
       <Header theme={theme} setTheme={setTheme} />
       <div>포트폴리오 올릴 예정</div>
     </>
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import { Container, Nav, Navbar, NavDropdown, ToggleButton } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import styles from "../css/Header.module.css";
+import { GoMoon, GoSun } from "react-icons/go";
 
 function Header(props) {
   const { theme, setTheme } = props;
-  const [checked, setChecked] = useState(false);
-  const handleThemeChange = (e) => {
-    setChecked(e.currentTarget.checked);
+  const handleThemeChange = () => {
     if (theme === "dark") {
       setTheme("light");
       localStorage.setItem("theme", "light");
@@ -18,7 +16,7 @@ function Header(props) {
     }
   };
   return (
-    <Navbar expand='lg' className='bg-body-tertiary' data-bs-theme={theme}>
+    <Navbar expand='lg' className='bg-body-tertiary' data-bs-theme={theme} sticky='top'>
       <Container>
         <Navbar.Brand href='#home'>윤정현 - 포트폴리오</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
@@ -30,9 +28,9 @@ function Header(props) {
             <Nav.Link href='#link'>Projects</Nav.Link>
             <Nav.Link href='#link'>Certifications</Nav.Link>
           </Nav>
-          <ToggleButton id='toggle-check' type='checkbox' variant='outline-primary' checked={checked} value='1' onChange={(e) => handleThemeChange(e)}>
-            {theme}
-          </ToggleButton>
+          <button className={styles.themeBtn} onClick={handleThemeChange}>
+            {theme === "dark" ? <GoMoon /> : <GoSun />}
+          </button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
