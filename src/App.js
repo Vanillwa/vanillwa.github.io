33bca3a970e7b8d9d1eecf0ed77146a2fcec4d30
Vanillwa <yjh30448813@gmail.com>
2024-09-18 13:54:27 +0900

    섹션1 배경이미지 / 하단 프로그레스 바

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Section1 from "./components/Section1";
 import Navi from "./components/Navi";
 import Section2 from "./components/Section2";
+import { motion, useScroll } from "framer-motion";
+import "./css/App.css";
 
 function App() {
   const [theme, setTheme] = useState(localStorage.theme);
+  const { scrollYProgress } = useScroll();
   useEffect(() => {
     if (localStorage.theme === "dark") document.documentElement.classList.add("dark");
   }, []);
   return (
     <>
+      <div className='bg'></div>
       <Navi theme={theme} setTheme={setTheme} />
+      <motion.div className='progress-bar' style={{ scaleX: scrollYProgress }} />
       <Section1 />
-
       <Section2 />
     </>
   );
