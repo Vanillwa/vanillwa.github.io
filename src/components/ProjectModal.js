77bfb43e626e67415bba051ea18b4aedc7bafa0b
Vanillwa <yjh30448813@gmail.com>
2024-09-28 21:44:57 +0900

    프로젝트 모달

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -1,18 +1,91 @@
+import { IoClose, IoPeopleSharp } from "react-icons/io5";
 import styles from "../css/ProjectModal.module.css";
+import { IoIosLink } from "react-icons/io";
+import { HiWrenchScrewdriver } from "react-icons/hi2";
+import { FaCalendarCheck, FaFileAlt, FaWrench } from "react-icons/fa";
+import { MdOpenInNew } from "react-icons/md";
 
-function ProjectModal({ handleCloseModal }) {
+function ProjectModal({ handleCloseModal, project }) {
   return (
-    <div className={styles.modal}>
+    <>
       <div className={styles.mask} onClick={handleCloseModal}></div>
-      <div className={styles.modalBody}>
-        <div className={styles.modalContent}>
-          모달입니다.
-          <button type='button' onClick={handleCloseModal}>
-            닫기
-          </button>
+      <div className={styles.modal}>
+        <div className={styles.modalContainer}>
+          <div className={styles.closeBtnWrapper}>
+            <IoClose className={styles.closeBtn} onClick={handleCloseModal} />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.titleWrapper}>
+              <div className={styles.title}>{project.title}</div>
+              <div className={styles.subtitle}>{project.subtitle}</div>
+            </div>
+            <div className={styles.rowWrapper}>
+              <div className={styles.row}>
+                <div className={styles.rowHeader}>
+                  <FaCalendarCheck />
+                  <span>기간</span>
+                </div>
+                <div className={styles.rowContent}>{project.term}</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.rowHeader}>
+                  <IoPeopleSharp />
+                  <span>인원</span>
+                </div>
+                <div className={styles.rowContent}>{project.member}</div>
+              </div>
+              <div className={styles.row}>
+                <div className={styles.rowHeader}>
+                  <HiWrenchScrewdriver />
+                  <span>기술 스택</span>
+                </div>
+                <div className={styles.rowContent}>
+                  {project.skillsText.map((el, index) => {
+                    return <span key={index}>{el}</span>;
+                  })}
+                </div>
+              </div>
+              {project.url.map((el, index) => {
+                return (
+                  <div key={index} className={styles.row}>
+                    <div className={styles.rowHeader}>
+                      <IoIosLink />
+                      <span>URL{index + 1}</span>
+                    </div>
+                    <div className={styles.rowContent}>
+                      <a href={el} target='_blank' rel='noreferrer noopener'>
+                        {el}
+                        <MdOpenInNew />
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+              <div className={styles.row}>
+                <div className={styles.rowHeader}>
+                  <FaFileAlt />
+                  <span>상세 설명</span>
+                </div>
+                <div className={styles.rowContent}>
+                  <a href={project.pdf} target='_blank' rel='noopener noreferrer'>
+                    PDF 파일 보기
+                    <MdOpenInNew />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div className={styles.comment}>
+              <div className={styles.commentTitle}>
+                <FaWrench />
+                <span>어려웠던 점</span>
+              </div>
+              <pre className={styles.commentContent}>{project.diff}</pre>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Section4.js
+++ src/components/Section4.js
@@ -10,6 +10,8 @@ import ProjectModal from "./ProjectModal";
 import { ModalPortal } from "./ModalPortal";
 
 function Section4() {
+  const [project, setProject] = useState(null);
+
   const items = [
     {
       image: image_rut,
@@ -17,7 +19,20 @@ function Section4() {
       subtitle: "mbti 게시판 및 채팅 사이트",
       term: "2024/04/15 ~ 2024/06/01",
       skills: [<FaReact />, <FaNodeJs />, <SiMysql />],
+      skillsText: ["React", "Node.js", "MySQL"],
       url: ["https://github.com/Vanillwa/mbti-node", "https://github.com/Vanillwa/mbti-react"],
+      member: "본인 외 3명",
+      diff: `1. CORS 문제를 해결하기 위해 HTTPS 서버를 구축해서 프론트와 서버의 API 통신을 유도했지만 완벽하게 해결되지 않았습니다.
+
+2. 후일의 프로젝트에선 프로젝트 완성 후 배포하는 과정에 있어서 도메인도 구매하고 SSL 인증서를 발급 받아 완벽한 HTTPS 서버를 만들어 보고 싶습니다.
+
+3. 웹 소켓을 사용해서 1대 1 실시간 채팅을 구현해보았습니다. 이후에 1대 1 뿐만이 아니라 3명 이상이 한번에 채팅이 가능한 그룹 채팅 방도 구현을 해보고 싶습니다.
+
+4. 얼마나 많은 사용자가 이번에 제작한 웹 서비스를 동시에 이용할 수 있는지 부하 테스트도 진행해보고 싶은 목표가 생겼습니다.
+
+두 번째 팀 프로젝트 또한 팀장으로 진행을 하게 되었습니다. 팀원들의 실력을 파악하고 적절하게 업무를 할당하니 프로젝트가 차질 없이 기한 내에 완성될 수 있었던 것 같습니다.
+이번 프로젝트를 통해 어떻게 개발자들이 협업을 하고 소통을 하는지 알 수 있었고 또 다른 프로젝트를 진행하게 될 때 좀 더 수월하게 진행할 수 있게 되었습니다.`,
+      pdf: "/pdf/RUT.pdf",
     },
     {
       image: image_gng,
@@ -25,20 +40,33 @@ function Section4() {
       subtitle: "반려동물 게시판 및 쇼핑몰 사이트",
       term: "2023/11/27 ~ 2023/12/18",
       skills: [<SiSpring />, <SiJquery />, <SiOracle />],
+      skillsText: ["Spring", "JQuery", "OracleDB"],
       url: ["https://github.com/Vanillwa/Gang_and_Go"],
+      member: "본인 외 3명",
+      diff: `무한 대댓글 기능을 구현하기 위해 DB 설계부터 시작했지만 원하는 모델을 얻기가 정말 어려웠습니다. 
+
+단순히 댓글에 대댓글을 작성하는 기능은 구현하기 쉬웠지만, 대댓글의 대댓글을 작성하기 시작하면서 부터 댓글 리스트의 정렬 순서가 기존의 사용하던 로직으로는 올바르게 배치가 불가능 했습니다.
+
+기존 댓글 테이블의 컬럼은 ref (참조하는 최상위 부모 댓글), depth (들여쓰기 깊이), step (한 댓글 그룹내의 순서 )으로 이루어져 있었습니다. 하지만 이러한 구조는 무한 대댓글 구조를 표현하기에 문제가 있었고 대댓글을 작성할 때 엉뚱한 댓글에 대댓글이 달리고 정렬이 역순으로 되는 등 문제점이 발생했습니다.
+
+이러한 문제를 해결하기 위해 테이블에 두 가지의 컬럼을 추가했습니다.
+기존 컬럼에 더해 nested (해당 댓글이 가지고 있는 자식 댓글의 수), parent (최상위 댓글이 아닌 바로 직속 부모 댓글) 컬럼을 추가하여 무한 대댓글을 구현할 수 있었습니다.`,
+      pdf: "/pdf/GnG.pdf",
     },
   ];
 
   const [show, setShow] = useState(false);
 
+  const handleShowModal = (item) => {
+    setProject(item);
+    document.body.style.overflow = "hidden";
+    setShow(true);
+  };
   const handleCloseModal = () => {
     document.body.style.overflow = "";
     setShow(false);
   };
-  const handleShowModal = () => {
-    document.body.style.overflow = "hidden";
-    setShow(true);
-  };
+
   return (
     <>
       <section className={styles.sec}>
@@ -47,7 +75,16 @@ function Section4() {
           <div className={styles.itemList}>
             {items.map((item, index) => {
               return (
-                <motion.div key={index} className={styles.item} initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.2 } }} viewport={{ amount: 0.8, once: true }}>
+                <motion.div
+                  key={index}
+                  className={styles.item}
+                  initial={{ opacity: 0, y: 30 }}
+                  whileInView={{ opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.2 } }}
+                  viewport={{ amount: 0.8, once: true }}
+                  whileHover={{ scale: 0.98, transition: { type: "tween", duration: 0.1 } }}
+                  onClick={() => {
+                    handleShowModal(item);
+                  }}>
                   <div className={styles.imageBox} style={{ backgroundImage: `url(${item.image})` }}></div>
                   <div className={styles.contentBox}>
                     <div className={styles.itemTitle}>{item.title}</div>
@@ -74,12 +111,9 @@ function Section4() {
               );
             })}
           </div>
-          <button type='button' onClick={handleShowModal}>
-            Open
-          </button>
           {show && (
             <ModalPortal>
-              <ProjectModal handleCloseModal={handleCloseModal} />
+              <ProjectModal handleCloseModal={handleCloseModal} project={project} />
             </ModalPortal>
           )}
         </Container>
